b7b51c05908b8aeb3e1c3004e9ffe59687004009
Podexus23 <santon23@gmail.com>
2023-09-04 22:50:48 +0300

    feat: add sass folder system

--- src/js/model.js
+++ src/js/model.js
@@ -1,7 +1,23 @@
-import { systems } from "./data.js";
+import { systems, allSystems } from "./systemsData.js";
+
+const mainContainer = document.querySelector(".container");
+const mapContainer = document.querySelector(".map");
+const header = document.querySelector(".header");
+
+const mapBlocks = allSystems;
+
+const addMapBlocks = function () {
+  mapBlocks.forEach((block) => {
+    const div = document.createElement("div");
+    div.classList.add("map__block");
+    div.textContent = block;
+    mapContainer.append(div);
+  });
+};
 
 const init = function () {
   console.log(systems);
+  addMapBlocks();
 };
 
 export { init };
--- src/main.js
+++ src/main.js
@@ -2,20 +2,4 @@
 
 import { init } from "./js/model.js";
 
-const mainContainer = document.querySelector(".container");
-const mapContainer = document.querySelector(".map");
-const header = document.querySelector(".header");
-
-const mapBlocks = new Array(20).fill("Name of system");
-
-const addMapBlocks = function () {
-  mapBlocks.forEach((block) => {
-    const div = document.createElement("div");
-    div.classList.add("map__block");
-    div.textContent = block;
-    mapContainer.append(div);
-  });
-};
-addMapBlocks();
-
 init();

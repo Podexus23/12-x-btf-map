ec3ef639e04979147e7eaa69635ab224ee7d169f
Podexus23 <santon23@gmail.com>
2023-09-19 18:21:41 +0300

    feat: add side lines between systems

--- src/js/model.js
+++ src/js/model.js
@@ -3,8 +3,9 @@ import { races } from "./data/racesData.js";
 import { mapColSize, mapRowSize, mapSize } from "./config.js";
 
 const mainContainer = document.querySelector(".container");
-const mapContainer = document.querySelector(".map");
 const header = document.querySelector(".header");
+
+const mapContainer = document.querySelector(".map");
 
 /**
  * Create empty blocks for the map
@@ -18,31 +19,41 @@ const createMapBlocks = function (numOfBlocks) {
 
     const div = document.createElement("div");
     div.classList.add("map__block");
-    div.textContent = `block`;
+    // div.textContent = `block`;
     div.dataset.coords = `${rowCoord},${columnCoord}`;
 
     mapContainer.append(div);
   }
 };
+
 /**
- * Add data to map block from Data about systems
+ * Add data to map block with Data from systems.js
  */
+//Add content from data to generated block
+const addContentToBlock = function (block, nameOfSystem) {
+  const data = systems[`${nameOfSystem}`];
+  // block.textContent = nameOfSystem;
+  // if (races.includes(data.race))
+  //   block.classList.add(`map__block--${data.race.toLowerCase()}`);
+  let content = `
+  <div class="wrapper map__block--${data.race.toLowerCase()}">
+            <p>${nameOfSystem}</p>
+  </div>
+  <span class="map__block--side map__block--side-e"></span>
+  <span class="map__block--side map__block--side-w"></span>
+  <span class="map__block--side map__block--side-n"></span>
+  <span class="map__block--side map__block--side-s"></span>
+  `;
+  block.insertAdjacentHTML("afterbegin", content);
+};
 
 const generateMap = function () {
   const names = Object.keys(systems);
   //take all blocks
   let blocks = mapContainer.querySelectorAll(".map__block");
 
-  //Add content from data to generated block
-  const addContentToBlock = function (block, nameOfSystem) {
-    const data = systems[`${nameOfSystem}`];
-    block.textContent = nameOfSystem;
-    if (races.includes(data.race))
-      block.classList.add(`map__block--${data.race.toLowerCase()}`);
-  };
-
   //Check if block data have same coordinates as system in data
-  //if true - add content, if false make block hidden
+  //if true - add content, else add hidden class
   blocks.forEach((block) => {
     const [x, y] = block.dataset.coords.split(",");
     let isEmpty = true;
@@ -52,12 +63,13 @@ const generateMap = function () {
 
       if (+sysX === +x && +sysY === +y) {
         addContentToBlock(block, name);
-        //remove name of found system for faster search
+        //remove name of found system for better search
         names.splice(i, 1);
         isEmpty = false;
         return;
       }
     });
+
     if (isEmpty) block.classList.add("hidden");
   });
 };
